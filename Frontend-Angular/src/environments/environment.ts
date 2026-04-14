07e376ea57b0b464774144fdc6759fd91b76fd84
https-Contreras <contrerasjael061@gmail.com>
2026-04-14 15:27:31 -0600

    FIX: APIurl

--- Frontend-Angular/src/environments/environment.ts
+++ Frontend-Angular/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'node-back-production-4aad.up.railway.app/api' // TODO: Reemplaza con la URL real de tu backend en producción
+  apiUrl: 'https://node-back-production-4aad.up.railway.app/api' // TODO: Reemplaza con la URL real de tu backend en producción
 };
